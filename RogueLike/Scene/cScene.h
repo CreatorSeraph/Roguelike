cd490f99ab0d1e7a2db3d5213f59b7059beeb626
creatorSeraph <dbgksdl00@gmail.com>
2019-12-17 04:31:02 +0900

    cSceneManager추가

--- RogueLike/Scene/cScene.h
+++ RogueLike/Scene/cScene.h
@@ -1,5 +1,13 @@
 #pragma once
-class cScene
+class cScene abstract
 {
+public:
+    cScene() = default;
+    virtual ~cScene() = default;
+
+    virtual void Init() PURE;
+    virtual void Update() PURE;
+    virtual void Render() PURE;
+    virtual void Destroy() PURE;
 };
 
--- RogueLike/Scene/cSceneManager.h
+++ RogueLike/Scene/cSceneManager.h
@@ -1,5 +1,22 @@
 #pragma once
-class cSceneManager
+#include <map>
+
+class cScene;
+class cSceneManager final
 {
+public:
+    MAKE_SINGLETONFUNC(cSceneManager, GetInst)
+private:
+    cScene* m_nowScene;
+    cScene* m_nextScene;
+    std::map<wstring, cScene*> m_scenes;
+public:
+    cSceneManager();
+    ~cSceneManager();
+
+    cScene* AddScene(const wstring_view& _name, cScene* _scene);
+    cScene* ChangeScene(const wstring_view& _name);
+    cScene* FindScene(const wstring_view& _name);
 };
 
+#define g_Scene cScene::GetInst()
